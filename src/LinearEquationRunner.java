ec3ba0132d622363eb269644a85e13328e620985
bths-ax <ardenx@nycstudents.net>
2022-10-20 08:45:13 -0400

    and finished like the rest of the program

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -12,10 +12,29 @@ public class LinearEquationRunner {
 		System.out.print("Enter coordinate 2: ");
 		Coordinate coord2 = new Coordinate(scanner.nextLine());
 
-		// testing
-		System.out.println(coord1.getX());
-		System.out.println(coord1.getY());
-		System.out.println(coord2.getX());
-		System.out.println(coord2.getY());
+		System.out.println();
+
+		// stupid edge case of vertical lines
+		if (coord1.getX() == coord2.getX()) {
+			System.out.println(String.format("These points are on a vertical line: x = %d", coord1.getX()));
+			return;
+		}
+
+		// create and print equation
+		LinearEquation equation = new LinearEquation(
+			coord1.getX(),
+			coord1.getY(),
+			coord2.getX(),
+			coord2.getY());
+
+		System.out.println(equation.lineInfo());
+		System.out.println();
+
+		// get alternate x value and print resulting y value
+		System.out.print("Enter a value for x: ");
+		double altX = scanner.nextDouble();
+
+		System.out.println();
+		System.out.println("The on the line is " + equation.coordinateForX(altX));
 	}
 }
