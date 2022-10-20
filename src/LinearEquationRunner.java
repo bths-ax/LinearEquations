c487281c738768cc3149e3e19c271ce0c9a9da29
bths-ax <ardenx@nycstudents.net>
2022-10-20 08:09:58 -0400

    testing stuff
    also screw you too "najamsr"

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class LinearEquationRunner {
 	public static void main(String[] args) {
-		// TODO: like everything lmao
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Welcome!");
+
+		// get coordinates
+		System.out.print("Enter coordinate 1: ");
+		Coordinate coord1 = new Coordinate(scanner.nextLine());
+		System.out.print("Enter coordinate 2: ");
+		Coordinate coord2 = new Coordinate(scanner.nextLine());
+
+		// testing
+		System.out.println(coord1.getX());
+		System.out.println(coord1.getY());
+		System.out.println(coord2.getX());
+		System.out.println(coord2.getY());
 	}
 }
